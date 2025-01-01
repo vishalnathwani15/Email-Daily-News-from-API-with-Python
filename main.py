e6309b191a0f070780e3d4625b5534ea81ed7f91
Vishal <vishal@gmail.com>
2025-01-01 12:49:52 +0530

    set email for same

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import requests
 from send_email import send_email 
 
-api_key = "038e5f6ccdc14b74997caa208d508650"
-url = "https://newsapi.org/v2/everything?q=apple&from=2024-12-27&to=2024-12-27&sortBy=popularity&apiKey=038e5f6ccdc14b74997caa208d508650"
+api_key = "038e5f6ccdc14b74997caa208d508650" # taking from https://newsapi.org link
+url = "https://newsapi.org/v2/everything?q=apple&from=2024-12-27&to=2024-12-27&sortBy=popularity&apiKey=038e5f6ccdc14b74997caa208d508650&language=en"
 
 # url = "https://finance.yahoo.com"
 
 request =requests.get(url)
 # content=request.textcontent =request.json # for creating str
 content =request.json() # for creating dictionary
+# check dictionary for same
+# print(content)
 
 body = " "
 
-for contents in content["articles"]:
+for contents in content["articles"][0:21]:
     if contents["title"] and contents["description"] is not None:
-        body = body + contents["title"] + "\n" + contents["description"] + 2*"\n"
+        body ="Subject: Today's News"  \
+        + "\n" + body + contents["title"] + "\n" \
+        + contents["description"] + "\n" + contents["url"] + 2*"\n"
 
 # print(body)
 body =body.encode("UTF-8")
